a77c7d5723f27b3ea6e283eb035ff9234f166430
liujunjuntj <liujunjuntj@163.com>
2015-06-25 18:15:08 +0800

    update by yuanyihui
    update by yuanyihui

--- BAT-master/Home/Lib/Action/ModuleAction.class.php
+++ BAT-master/Home/Lib/Action/ModuleAction.class.php
@@ -1,30 +1,30 @@
 <?php
 class ModuleAction extends CommonAction{
-	
-	public function mlist(){
-		$name = $this->_get('name');
-		if (!empty($name)) {
-			$condition['name'] = array('like','%'.trim($name).'%');
-		}
-		$condition = $this->merge($condition);
-		//分页
-		$page = $this->getPaging('Module', $condition);
-		$paging = $page->show();
-		$this->assign('paging', $paging);
-		//根据查询条件获取数据
-		$modules = D('Module')->where($condition)->order('createTime desc')->limit($page->firstRow . ',' . $page->listRows)->select();
-		$this->assign('datas', $modules);
-		$this->display();
-	}
-	
-	public function modules(){
-		$where['status'] = C("VALID");
-		$where['appId'] = getAppId();
-		$modules = D("Module")->getModule($where);
+
+    public function mlist(){
+        $name = $this->_get('name');
+        if (!empty($name)) {
+            $condition['name'] = array('like','%'.trim($name).'%');
+        }
+        $condition = $this->merge($condition);
+        //分页
+        $page = $this->getPaging('Module', $condition);
+        $paging = $page->show();
+        $this->assign('paging', $paging);
+        //根据查询条件获取数据
+        $modules = D('Module')->where($condition)->order('createTime desc')->limit($page->firstRow . ',' . $page->listRows)->select();
+        $this->assign('datas', $modules);
+        $this->display();
+    }
+
+    public function modules(){
+        $where['status'] = C("VALID");
+        $where['appId'] = getAppId();
+        $modules = D("Module")->getModule($where);
         //dump($modules);exit;
-		$this->assign("datas",$modules);
-		$this->ajaxReturn($this->fetch(),"成功",'success:true');
-	}
+        $this->assign("datas",$modules);
+        $this->ajaxReturn($this->fetch(),"成功",'success:true');
+    }
     /**
      * 新增module
      */
@@ -71,8 +71,8 @@ class ModuleAction extends CommonAction{
             //判断module名是否重复（同一app下不能重复）
             if($module['apps'][$i]!=$currnet_appId) {
                 $condition['name'] = $module['name'];
-                $condition['status'] = C('VALID');
                 $condition['appId'] = $module['apps'][$i];
+                $condition['status'] = C('VALID');
                 $temp = D('Module')->checkDuplicate($condition);
                 if (!empty($temp)) {
                     $this->ajaxReturn(0, $apps[$i] . "下已有同名的模块，不允许重复录入", "success:false");
@@ -86,7 +86,7 @@ class ModuleAction extends CommonAction{
                 }
             }
         }
-            $this->ajaxReturn($ret, "成功新增模块数据！", "success:true");
+        $this->ajaxReturn($ret, "成功新增模块数据！", "success:true");
     }
 
     /**
@@ -98,25 +98,10 @@ class ModuleAction extends CommonAction{
         if(empty($ret)){
             $this->error("ID" . $id . "对应的Module不存在！！！", "mlist", 2);
         }
-        $condition['name'] = $ret['name'];
-        $addAppsNum = D('Module')->where($condition)->count();
-        if($addAppsNum > 1){
-            $ret["addedApp"] = array();
-            $modules = D('Module')->where($condition)->select();
-            foreach ($modules as $module) {
-                $app = D('App')->getById($module['appId']);
-                array_push($ret["addedApp"], $app["appName"]);
-            }
-        }
         $this->assign('module',$ret);
-
-        //获得全部app信息
-        $apps = D('App')->getAllApps();
-        $this->assign('allApps',$apps);
 
         //根据当前用户id查询相关app信息
         $this->assign("currentApp",$_SESSION['appName']);
-
         $this->display();
     }
 
@@ -132,62 +117,60 @@ class ModuleAction extends CommonAction{
         //判断传入参数是否符合格式
         $module = D('Module')->checkModule($module);
 
-        $defaultAppId = $_SESSION['appId'];
-        $defaultAppName = $_SESSION['appName'];
-        $module['appId'] = $defaultAppId;
-        $module['addedApps'] = $_POST['addedApps'];
-        $apps = explode(' ',trim($module['addedApps']));
-        $temp = D('Module')->checkDuplicate($module);
+        $module['appId'] = $_SESSION['appId'];;
+
+        //查重条件
+        $condition['id'] = array('NEQ', $module['id']);
+        $condition['name'] = $module['name'];
+        $condition['appId'] = $module['appId'];
+        $condition['status'] = C('VALID');
+        $temp = D('Module')->checkDuplicate($condition);
         if (!empty($temp)) {
             $this->ajaxReturn(0, "已经有相同的module存在，不允许重复录入", "success:false");
         }
-        //执行更新,先删后加
-        $condition['name'] = D('Module')->getModuleNameByModuleId($module['id']);
-        $temp = D('Module')->where($condition)->delete();
+
+        //执行更新
+        $data['status'] = C('INVALID');
+        $where['id'] = $module['id'];
+        $temp = D('Module')->where($where)->save($module);
         if ($temp == false) {
             $this->ajaxReturn(0,"更新失败", "success:false");
         }
-        $ret = D('Module')->add($module);
-        if (!$ret) {
-            $this->ajaxReturn(0, "更新模块数据失败！", "success:false");
-        }
-        foreach($apps as $app){
-            if($app != $defaultAppName) {
-                $data['name'] = $module['name'];
-                $data['desc'] = $module['desc'];
-                $data['appId'] = D('App')->getIdByAppName($app);
-                $temp = D('Module')->checkDuplicate($data);
-                if (!empty($temp)) {
-                    $this->ajaxReturn(0, "已经有相同的module存在，不允许重复录入", "success:false");
-                }
-                $ret = D('Module')->add($data);
-                if (!$ret) {
-                    $this->ajaxReturn(0, "更新模块数据失败！", "success:false");
-                }
-            }
-        }
-        $this->ajaxReturn($ret, "成功更新模块数据！", "success:true");
+        $this->ajaxReturn($temp, "成功更新模块数据！", "success:true");
     }
 
     /**
-     * 删除module
+     * 删除module--软删除
      */
     public function delete(){
-        $ids = $this->_post('ids');
+        $id = $this->_post('ids');
+        $ids = explode(",", $id);
         if (empty($ids)) {
-            $this->ajaxReturn(0,"请选择待删除的Module。","success:false");
+            $this->ajaxReturn(0, "请选择待删除的Module。", "success:false");
         }
 
-        $names = array();
-        $ids = explode(',', $ids);
-        foreach($ids as $id){
-            array_push($names, D('Module')->getModuleNameByModuleId($id));
+        $condition['status'] = C('VALID');
+        $condition['appId'] = $_SESSION['appId'];
+        $condition['userId'] = $_SESSION['uid'];
+        foreach( $ids as $id ) {
+            //查找模块下的api和用例
+            $condition['mid'] = $id;
+
+            $apis = D('Api')->where($condition)->getField('id', true);
+            $cases = D('Case')->where($condition)->getField('id', true);
+
+            if(!empty($apis) || !empty($cases)){
+                $this->ajaxReturn($id, "删除模块[$id]失败,该模块下有api或者用例！","success:false");
+            }
+
+            //删除模块
+            $where['id'] = $id;
+            $data['status'] = C('INVALID');
+            $ret = D('Module')->where($where)->save($data);
+            if (empty($ret)) {
+                $this->ajaxReturn($ret, "删除Module失败！", "success:false");
+            }
         }
-        $condition["name"] = array("in", $names);
-        $ret = D('Module')->where($condition)->delete();
-        if (empty($ret)) {
-            $this->ajaxReturn($ret,"删除Module失败！","success:false");
-        }
-        $this->ajaxReturn($ret,"成功删除Module。","success:true");
+        $this->ajaxReturn(null, "成功删除Module。", "success:true");
     }
 }
--- BAT-master/Public/js/module.js
+++ BAT-master/Public/js/module.js
@@ -1,65 +1,65 @@
 function OnRightClick(event, treeId, treeNode) {
-	if (!treeNode && event.target.tagName.toLowerCase() != "button" && $(event.target).parents("a").length == 0) {
-		zTree.cancelSelectedNode();
-		showRMenu("root", event.clientX, event.clientY);
-	} else if (treeNode && !treeNode.noR) {
-		zTree.selectNode(treeNode);
-		showRMenu("node", event.clientX, event.clientY);
-	}
+    if (!treeNode && event.target.tagName.toLowerCase() != "button" && $(event.target).parents("a").length == 0) {
+        zTree.cancelSelectedNode();
+        showRMenu("root", event.clientX, event.clientY);
+    } else if (treeNode && !treeNode.noR) {
+        zTree.selectNode(treeNode);
+        showRMenu("node", event.clientX, event.clientY);
+    }
 }
 
 function showRMenu(type, x, y) {
-	$("#rMenu ul").show();
-	if (type=="root") {
-		$("#m_del").hide();
-		$("#m_check").hide();
-		$("#m_unCheck").hide();
-	} else {
-		$("#m_del").show();
-		$("#m_check").show();
-		$("#m_unCheck").show();
-	}
-	rMenu.css({"top":y+"px", "left":x+"px", "visibility":"visible"});
+    $("#rMenu ul").show();
+    if (type=="root") {
+        $("#m_del").hide();
+        $("#m_check").hide();
+        $("#m_unCheck").hide();
+    } else {
+        $("#m_del").show();
+        $("#m_check").show();
+        $("#m_unCheck").show();
+    }
+    rMenu.css({"top":y+"px", "left":x+"px", "visibility":"visible"});
 
-	$("body").bind("mousedown", onBodyMouseDown);
+    $("body").bind("mousedown", onBodyMouseDown);
 }
 function hideRMenu() {
-	if (rMenu) rMenu.css({"visibility": "hidden"});
-	$("body").unbind("mousedown", onBodyMouseDown);
+    if (rMenu) rMenu.css({"visibility": "hidden"});
+    $("body").unbind("mousedown", onBodyMouseDown);
 }
 function onBodyMouseDown(event){
-	if (!(event.target.id == "rMenu" || $(event.target).parents("#rMenu").length>0)) {
-		rMenu.css({"visibility" : "hidden"});
-	}
+    if (!(event.target.id == "rMenu" || $(event.target).parents("#rMenu").length>0)) {
+        rMenu.css({"visibility" : "hidden"});
+    }
 }
 var addCount = 1;
 function addTreeNode() {
-	hideRMenu();
-	var newNode = { name:"增加" + (addCount++)};
-	if (zTree.getSelectedNodes()[0]) {
-		newNode.checked = zTree.getSelectedNodes()[0].checked;
-		zTree.addNodes(zTree.getSelectedNodes()[0], newNode);
-	} else {
-		zTree.addNodes(null, newNode);
-	}
+    hideRMenu();
+    var newNode = { name:"增加" + (addCount++)};
+    if (zTree.getSelectedNodes()[0]) {
+        newNode.checked = zTree.getSelectedNodes()[0].checked;
+        zTree.addNodes(zTree.getSelectedNodes()[0], newNode);
+    } else {
+        zTree.addNodes(null, newNode);
+    }
 }
 function removeTreeNode() {
-	hideRMenu();
-	var nodes = zTree.getSelectedNodes();
-	if (nodes && nodes.length>0) {
-		if (nodes[0].children && nodes[0].children.length > 0) {
-			var msg = "要删除的节点是父节点，如果删除将连同子节点一起删掉。\n\n请确认！";
-			if (confirm(msg)==true){
-				zTree.removeNode(nodes[0]);
-			}
-		} else {
-			zTree.removeNode(nodes[0]);
-		}
-	}
+    hideRMenu();
+    var nodes = zTree.getSelectedNodes();
+    if (nodes && nodes.length>0) {
+        if (nodes[0].children && nodes[0].children.length > 0) {
+            var msg = "要删除的节点是父节点，如果删除将连同子节点一起删掉。\n\n请确认！";
+            if (confirm(msg)==true){
+                zTree.removeNode(nodes[0]);
+            }
+        } else {
+            zTree.removeNode(nodes[0]);
+        }
+    }
 }
 
 function editTreeNode(){
-	console.info("edit node");
+    console.info("edit node");
 }
 
 var zTree, rMenu;
@@ -86,6 +86,7 @@ $(document).ready(function(){
     //修改模块提交表单
     $("#module_update_form").submit(doUpdate);
     //删除module
+    //$("#module_delete").removeAttr('disabled', 'true');
     $("#module_delete").click(function(){
         var checked_num = $(".selectCell:checked").length;
         if (checked_num < 1) {
@@ -115,10 +116,9 @@ $(document).ready(function(){
                         location.reload();
                     }
                     if(status == 'success:false'){
-                        showError(result.info);
-                        return false;
+                        showInfo(result.info);
                     }
-                },
+                }
             });
         });
     });
