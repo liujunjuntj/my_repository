7bd92ccbd79831079980769c1455a7bcf67fca7a
liujunjuntj <liujunjuntj@163.com>
2015-07-01 17:36:20 +0800

    assert update
    断言部分添加key-value规则界面部分

---
+++ BAT-master/Public/js/assert.js
@@ -0,0 +1,53 @@
+//添加KEYVALUE断言操作
+$('.add-rule-assert').unbind('click').click(function(){
+    getKeyValueTmpl('keyvalue','',$(this));
+});
+
+$("input[name=assert_type]").unbind('click').click(function(){
+    switch ($("input[name=assert_type]:checked").attr("class")) {
+        case "assert_full":
+            $(this).parent().next().next().show();
+            $(this).parent().next().next().next().hide();
+            break;
+        case "assert_keyvalue":
+            $(this).parent().next().hide();
+            $(this).parent().next().next().show();
+            break;
+    }
+});
+var trmoveToUp = function(){
+    var move = $(this).closest(".assert_tr");
+    var prev = move.prev(".assert_tr");
+    prev.before(move);
+}
+//assert_tr步骤向下移动
+var trmoveToDown = function(){
+    var move = $(this).closest(".assert_tr");
+    var next = $(this).closest(".assert_tr").next(".assert_tr");
+    next.after(move);
+}
+//获取key-value模版的通用方法
+var getKeyValueTmpl = function(action,params,dom){
+    $.ajax({
+        type : "get",
+        url : APP + "/Component/" + action,
+        data : params,
+        success:function(result){
+            status = result.status;
+            if(status == 10001){
+                redirect('/Login/login');
+            }
+            if(status == "success:false"){
+                showError(result.info);
+                return false;
+            }
+            dom.parent().parent().parent().parent().append(result.data);
+            //绑定删除按钮事件
+            $(".icon-remove").click(function(){
+                $(this).closest(".assert_tr").remove();
+            });
+            $(".assert_tr .icon-circle-arrow-up").last().click(trmoveToUp);
+            $(".assert_tr .icon-circle-arrow-down").last().click(trmoveToDown);
+        }
+    });
+}
